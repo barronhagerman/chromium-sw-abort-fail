54413e6907be987fd8d4265b93bb5cdacb45bb05
Barron Hagerman <barron.hagerman@banno.com>
2019-09-18 16:47:55 -0500

    add simple Express web server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const root = 'dist';
+
+app.get('/', (req, res) => res.sendFile('index.html', {root}));
+app.get(/^(.+)$/, (req, res) => res.sendFile(req.params[0], {root}));
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
