d3f075ce35a094419c5a97c8de73c8b403bb2aff
Barron Hagerman <barron.hagerman@banno.com>
2019-09-19 11:27:25 -0500

    add HTTP cookie to illustrate security concern
    * Updates `GET /` to set an HTTP cookie on page load
    * Updates `GET /delayedApiResponse` to set an HTTP cookie to illustrate
    security concern with this bug
    * Adds `GET /logout` to immediately clear any previously created
    cookie

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const app = express();
 const port = 3000;
 const root = 'dist';
 
-app.get('/', (req, res) => res.sendFile('index.html', {root}));
+const cookieName = 'CookieCreatedAfterAbort';
 
 app.get('/delayedApiResponse', (req, res) => {
   setTimeout(() => {
+    res.header('Set-Cookie', `${cookieName}=blahblahblah;Path=/;HTTPOnly`);
     res.send();
-  }, 5000)
+  }, 5000);
 });
 
+app.get('/logout', (req, res) => {
+  res.header('Set-Cookie', `${cookieName}=;Max-Age=0;Expires=Thu, 01 Jan 1970 00:00:00 GMT;Path=/;HTTPOnly`);
+  res.send();
+});
+
+app.get('/', (req, res) => {
+  res.header('Set-Cookie', `${cookieName}=blahblahblah;Path=/;HTTPOnly`);
+  res.sendFile('index.html', {root});
+});
 app.get(/^(.+)$/, (req, res) => res.sendFile(req.params[0], {root}));
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
