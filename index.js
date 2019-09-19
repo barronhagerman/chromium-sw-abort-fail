3a0e3f63cb9c7c77854c9b7d95758191a05dbbd2
Barron Hagerman <barron.hagerman@banno.com>
2019-09-19 11:16:25 -0500

    add request to exemplify bug
    * Adds endpoint `GET /delayedApiCall`, whose response is delayed by 5
    seconds
    * Adds example request to new endpoint as soon as the service worker is
    ready and aborts it after 400ms (far shorter than the 5000ms delayed
    response) to show how Chrome processes the headers received by an aborted
    request when a `ServiceWorker` is active

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port = 3000;
 const root = 'dist';
 
 app.get('/', (req, res) => res.sendFile('index.html', {root}));
+
+app.get('/delayedApiResponse', (req, res) => {
+  setTimeout(() => {
+    res.send();
+  }, 5000)
+});
+
 app.get(/^(.+)$/, (req, res) => res.sendFile(req.params[0], {root}));
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
